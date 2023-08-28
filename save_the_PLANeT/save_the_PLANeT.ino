867512ae8bc81616e55bf4cc60a6470f1df3cba1
hakfossa <111423987+hakfossa@users.noreply.github.com>
2023-08-28 10:49:24 +0200

    Added UNO code file
    No content yet, only comment on what program is for.

---
+++ save_the_PLANeT/save_the_PLANeT.ino
@@ -0,0 +1,12 @@
+/* Code for the Arduino UNO part of the watering system. Will execute all automated functions
+and communicate with the Arduino MKR1010 to keep the website updated */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
